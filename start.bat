19ffaff7a0f58fcc771af71c8e319dee6d16552c
Artem Strelkov <a.strelkov@krds.ru>
2023-01-11 10:15:11 +0600

    feat: added bridge prompt

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+TITLE AVITOBOT
 setlocal ENABLEDELAYEDEXPANSION
 
 DEL .env
@@ -15,10 +16,24 @@ ECHO [%%v]
 SET /p value=Enter value for %%v: && ECHO %%v=!value!>>.env
 )
 
+DEL bot/torrc
+TYPE > bot/torrc
+
+SET bridgetemplate=Bridge obfs4
+
+FOR /f "delims=" %%v IN (torrc-template.txt) DO (
+    ECHO "%%v %bridgetemplate%"
+    IF /i "%%v" == "%bridgetemplate%" (
+        SET /p userbridge="Enter bridge" && ECHO %%v !userbridge!>>bot/torrc
+    ) else (
+        ECHO %%v>>bot/torrc
+    )
+)
+
 ECHO Is everything ok?
 TYPE .env
 PAUSE
-ECHO 
-SET /p botname="Enter bot name" && docker compose -p !botname! up --build
+ECHO " "
+SET /p botname="Enter bot name" && docker compose -p !botname! up -d --build
 PAUSE
 DEL .env
