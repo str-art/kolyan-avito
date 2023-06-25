efee861c47ecf6acad03be10f7b7cc18ef3498c0
str-art <art.strelkov@horoshiki.ru>
2023-06-25 23:58:40 +0600

    feat: telegram lambda handler

---
+++ aws-bot/src/telegram/index.js
@@ -0,0 +1,23 @@
+const { Telegram } = require("./telegram.js");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const tg = new Telegram();
+
+exports.handler = async (event, context) => {
+  try {
+    const { Records } = event;
+    const { dynamodb } = Records[0];
+    const item = unmarshall(dynamodb.NewImage);
+    console.log(dynamodb);
+    console.log(item);
+    await tg.sendMessage(item);
+    return {
+      code: 200,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      code: 500,
+    };
+  }
+};
---
+++ aws-bot/src/telegram/telegram.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+class Telegram {
+  constructor() {
+    this.host = "https:/api.telegram.org/";
+  }
+
+  async sendMessage(item) {
+    const text = this.formatText(item);
+    await this.request(item.token, "post", "sendMessage", {
+      chat_id: item.chat_id,
+      text,
+      parse_mode: "HTML",
+    });
+  }
+
+  async request(token, method, action, body = {}, params = {}) {
+    return axios({
+      method: method.toLowerCase(),
+      baseURL: this.host,
+      url: `bot${token}/${action}`,
+      data: body,
+      params,
+    });
+  }
+
+  formatText({ price, title, url, description }) {
+    return `<b>${title}</b>
+        <b>Цена: ${price}</b>
+        <a href="${url}"> Ссылка </a>
+        ${description}
+        `;
+  }
+}
+
+module.exports = {
+  Telegram,
+};
