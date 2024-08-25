b244b8b8747f37c8c4d1691852d92e71415079e2
Artyom Strelkov <astrelkov@klika-tech.com>
2024-08-25 11:36:05 +0500

    feat: added database infra

---
+++ aws-bot/src/parser/infrastructure/modules/dynamo/dynamo.tf
@@ -0,0 +1,45 @@
+resource "aws_dynamodb_table" "this" {
+  name = local.table_name
+  tags = local.tags
+  billing_mode = "PROVISIONED"
+  read_capacity = 1
+  write_capacity = 1
+
+  hash_key = "PK"
+  range_key = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "price"
+    type = "N"
+  }
+
+  attribute {
+    name = "description"
+    type = "S"
+  }
+
+  attribute {
+    name = "chat_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "title"
+    type = "S"
+  }
+
+  attribute {
+    name = "url"
+    type = "S"
+  }
+}
